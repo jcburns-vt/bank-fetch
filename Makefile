46d96cdaa241554897aebc79460c9898e2f7e7f1
jc3urns <jc3urns@vt.edu>
2025-06-06 11:17:22 -0400

    refactored __main__.py

--- Makefile
+++ Makefile
@@ -1,17 +1,26 @@
 
-run:
+json:
 	export PYTHONPATH=src; \
 	python -m fetch --cert /etc/teller/certificate.pem \
-		--cert-key /etc/teller/private_key.pem ./data
+		--cert-key /etc/teller/private_key.pem ./data \
+		--debug --file-type json
+
+csv:
+	export PYTHONPATH=src; \
+	python -m fetch --cert /etc/teller/certificate.pem \
+		--cert-key /etc/teller/private_key.pem ./data \
+		--debug --file-type csv
 
 reset:
 	export PYTHONPATH=src; \
 	python -m fetch --cert /etc/teller/certificate.pem \
-		--cert-key /etc/teller/private_key.pem --reset ./data
+		--cert-key /etc/teller/private_key.pem --reset ./data \
+		--debug
 
 recent:
 	export PYTHONPATH=src; \
 	python -m fetch --cert /etc/teller/certificate.pem \
 		--cert-key /etc/teller/private_key.pem ./data \
-		--date-from 2025-06-01
+		--date-from 2025-06-01 \
+		--debug
 
--- src/fetch/__main__.py
+++ src/fetch/__main__.py
@@ -6,29 +6,44 @@ import requests
 import time
 import webbrowser
 import os
+import sys
+import logging
 
 from flask import Flask, render_template, request
 from multiprocessing import Process, Event
+from requests.models import HTTPError
+
+logger = logging.getLogger(__name__)
 
 def _parse_args():
     parser = argparse.ArgumentParser(
         prog="Teller Fetch",
-        description="Uses Teller API to fetch bank transactions"
+        description="Uses Teller API to fetch bank transactions",
+        epilog=(
+            "Basic usage: specify the output folder and the path to each of"
+            " your Teller credentials using --cert and --cert-key flags."
+        ),
     )
     parser.add_argument(
         '--date-from',
         type=str,
-        help="oldest record date in form yyyy-mm-dd",
+        help="oldest desired record date in form yyyy-mm-dd",
     ),
     parser.add_argument(
         '--date-to',
         type=str,
-        help="newest record date in form yyyy-mm-dd",
+        help="newest desired record date in form yyyy-mm-dd",
     ),
-    parser.add_argument('--cert', type=str,
-            help='path to the TLS certificate')
-    parser.add_argument('--cert-key', type=str,
-            help='path to the TLS certificate private key')
+    parser.add_argument(
+        '--cert',
+        type=str,
+        help='path to the TLS certificate'
+    )
+    parser.add_argument(
+        '--cert-key',
+        type=str,
+        help='path to the TLS certificate private key'
+    )
     parser.add_argument(
         '-r',
         '--reset',
@@ -36,8 +51,19 @@ def _parse_args():
         help='discard saved credentials and setup from scratch',
     )
     parser.add_argument(
+        '--file-type',
+        choices=["json", "csv"],
+        help="output file type"
+    )
+    parser.add_argument(
         'output_folder',
         help='path to desired output folder',
+    )
+    parser.add_argument(
+        '-d',
+        '--debug',
+        action='store_true',
+        help='enable debug logging'
     )
     args = parser.parse_args()
 
@@ -49,92 +75,157 @@ def _parse_args():
 
     return args
 
-base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
-templates_dir = os.path.join(base_dir, "templates")
-static_dir = os.path.join(base_dir, "static")
-app = Flask(__name__, template_folder=templates_dir, static_folder=static_dir)
-token_received_event = Event()
-def connect_account():
-    flask_process = Process(target=app.run)
-    flask_process.start()
-    webbrowser.open("http://127.0.0.1:5000")
-    token_received_event.wait()
-    time.sleep(.5)
-    flask_process.terminate()
-    flask_process.join()
+
+class MicroServer:
+    def __init__(self):
+        base_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../../")
+        )
+        templates_dir = os.path.join(base_dir, "templates")
+        static_dir = os.path.join(base_dir, "static")
+        self.app = Flask(
+            __name__,
+            template_folder=templates_dir,
+            static_folder=static_dir,
+        )
+        self.token_received_event = Event()
+        self._register_routes()
+
+    def connect_account(self):
+        flask_process = Process(target=self.app.run)
+        flask_process.start()
+        webbrowser.open("http://127.0.0.1:5000")
+        self.token_received_event.wait()
+        time.sleep(.5)
+        flask_process.terminate()
+        flask_process.join()
+
+    def _register_routes(self):
+
+        @self.app.route("/", methods=["GET"])
+        def home():
+            return render_template("index.html")
 
 
-@app.route("/", methods=["GET"])
-def home():
-    return render_template("index.html")
+        @self.app.route("/complete", methods=["POST"])
+        def complete():
+            access_token = request.get_json().get("access_token", None)
+            if access_token:
+                keyring.set_password("teller-fetch", "user", access_token)
+                self.token_received_event.set()
+                logger.info("Access token received successfully from web UI")
+                return "Access token received. You may now close this window."
+            else:
+                return "No access token received.", 400
 
 
-@app.route("/complete", methods=["POST"])
-def complete():
-    print(request.get_json())
-    access_token = request.get_json().get("access_token", None)
-    if access_token:
-        keyring.set_password("teller-fetch", "user", access_token)
-        token_received_event.set()
-        return "Access token received. You may now close this window."
-    else:
-        return "No access token received.", 400
+class Client:
+    def __init__(self, cert, cert_key, access_key):
+        self._cert = (cert, cert_key)
+        self._auth = (access_key, '')
+        self._base_url = "https://api.teller.io"
+
+    def get_accounts(self):
+        accounts_json = self.get(f"{self._base_url}/accounts")
+        accounts_list = [
+            Account(account, self)
+            for account in accounts_json
+        ]
+        return accounts_list
+
+
+    def get(self, url):
+        response = requests.get(
+            url=url,
+            cert=self._cert,
+            auth=self._auth,
+        )
+        try:
+            response.raise_for_status()
+        except HTTPError as err:
+            logger.error(err)
+            sys.exit(2)
+        return response.json()
+
+
+class Account:
+
+    def __init__(self, json, client):
+        self.name = json["name"]
+        self._transactions = client.get(
+            url=json["links"]["transactions"]
+        )
+
+    def get_transactions(self, date_from=None, date_to=None):
+
+        # discard outside of date range
+        pruned_transactions = []
+        for transaction in self._transactions:
+            date = transaction["date"]
+            if date_from:
+                if date < date_from:
+                    continue
+            if date_to:
+                if date > date_to:
+                    continue
+            pruned_transactions.append(transaction)
+
+        return pruned_transactions
+
+
+def dump_transactions_json(output_folder, out_file_name, transactions):
+    with open(
+        os.path.join(output_folder, out_file_name),
+        "w",
+    ) as file:
+        json.dump(transactions, file, indent=2)
+
+
+def dump_transactions_csv(output_folder, out_file_name, transactions):
+    pass
 
 
 def main():
+
     args = _parse_args()
     cert = (args.cert, args.cert_key)
     output_folder = args.output_folder
 
-    if args.reset:
-        keyring.delete_password("teller-fetch", "user")
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+    )
 
     access_key = keyring.get_password("teller-fetch", "user")
     if (not access_key):
-        connect_account()
+        logger.debug("no access_key found, requesting new one")
+        microserver = MicroServer()
+        microserver.connect_account()
+    elif access_key and args.reset:
+        keyring.delete_password("teller-fetch", "user")
+        logger.debug("access_key deleted")
 
     access_key = keyring.get_password("teller-fetch", "user")
-    print(access_key)
+    if not access_key:
+        logger.error("error acquiring access_key")
+        sys.exit(2)
+    logger.debug("successfully acquired access_key fom keyring")
 
-    response = requests.get(
-        url="https://api.teller.io/accounts",
-        cert=cert,
-        auth=(access_key, '')
-    )
-    response.raise_for_status()
+    client = Client(*cert, access_key)
+    accounts_list = client.get_accounts()
 
-    accounts_json = response.json()
-    for value in accounts_json:
+    for account in accounts_list:
+        out_file_name = f"{account.name}_transactions.json"
 
-        out_file_name = f"{value["name"]}_transactions.json"
-        transactions_href = value["links"]["transactions"]
-
-        # get transactions
-        response = requests.get(
-            url=transactions_href,
-            cert=cert,
-            auth=(access_key, ''),
+        transactions = account.get_transactions(
+            date_from=args.date_from if args.date_from else None,
+            date_to=args.date_to if args.date_to else None,
         )
-        transactions_json = response.json()
 
-        # discard outside of date range
-        pruned_transactions = []
-        for transaction in transactions_json:
-            date = transaction["date"]
-            if args.date_from:
-                if date < args.date_from:
-                    continue
-            if args.date_to:
-                if date > args.date_to:
-                    continue
-            pruned_transactions.append(transaction)
-
-        # dump pruned transactions to file
-        with open(
-            os.path.join(output_folder, out_file_name),
-            "w",
-        ) as file:
-            json.dump(pruned_transactions, file, indent=2)
+        if not args.file_type or args.file_type=="json":
+            dump_transactions_json(output_folder, out_file_name, transactions)
+            logger.info("dumping transactions to json file")
+        elif args.file_type=="csv":
+            logger.info("dumping transactions to csv file")
 
 
 if __name__ == "__main__":
