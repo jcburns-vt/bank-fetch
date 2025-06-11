fe27f1fe6940ad7b7a8fe8482b0a8151ba9fe837
jc3urns <jc3urns@vt.edu>
2025-06-10 22:12:29 -0400

    added a nickname argument to add command

--- src/fetch/cmd/commands/add.py
+++ src/fetch/cmd/commands/add.py
@@ -9,8 +9,16 @@ logger = logging.getLogger(__name__)
 def add_parser(subparsers, parents):
     add_cmd: ArgumentParser = subparsers.add_parser(
         "add",
+        usage="bf add [args] [options]",
         help="Connect a new banking institution",
         parents=parents,
+    )
+    add_cmd.add_argument(
+        "nickname",
+        help=(
+            "Nickname to be associated with bank connection. If none "
+            "specified, a random identifier will be assigned instead."
+        )
     )
     add_cmd.set_defaults(func=run)
 
